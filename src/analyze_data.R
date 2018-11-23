7068cf71aab375b6c05d41d4d8a114ec28c1f046
Wilson Deng <dengweishun@hotmail.com>
2018-11-23 11:10:04 -0800

    Add plot in analyze_data

--- src/analyze_data.R
+++ src/analyze_data.R
@@ -6,11 +6,12 @@
 # and perform a welch two sample t test
 # then stored the result as a csv file in results folder
 #
-# This script takes two arguments
-# Usage: Rscript src/analyze_data.R data/tidy_data.csv results/results.csv
+# This script takes three arguments
+# Usage: Rscript src/analyze_data.R data/tidy_data.csv results/results.csv results/testplot.png
 
 # load package
 suppressPackageStartupMessages(library(broom))
+suppressPackageStartupMessages(library(tidyverse))
 
 # define main function
 main <- function(){
@@ -18,6 +19,7 @@ main <- function(){
   args <- commandArgs(trailingOnly = TRUE)
   input <- args[1]
   csv_path <- args[2]
+  png_path <- args[3]
   
   # import data
   data <- read.csv(input)
@@ -25,8 +27,31 @@ main <- function(){
   # perform two-sample t test
   results <- tidy(t.test(PM_Average ~ city, data = data))
   
+  # Assign parameters
+  v = results[["parameter"]]
+  threshold = qt(0.975, v)
+  delta = results[["statistic"]]
+  
+  # Create a plot  
+  test_plot <- ggplot(data = data.frame(x = c(-5, 25)), aes(x)) + 
+    stat_function(fun = dt, n = 101, args = list(df = v)) +
+    geom_vline(xintercept = c(-threshold, threshold),
+               color = "blue",
+               lty = 2) +
+    geom_vline(xintercept = delta,
+               color = "red") + 
+    annotate("text", x = 16, y = 0.25,size = 5, label = paste("Test statistic:",
+                                                              round(delta,2),
+                                                              "\n df:",
+                                                              round(v,2)))
+  
   # Output csv
   write.csv(results, csv_path, row.names = FALSE)
+  
+  # Output png
+  png(png_path)
+  plot(test_plot)
+  dev.off()
 }
 
 # call main function
